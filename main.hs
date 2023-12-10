53a21ed28f3f137cb64d670a06cec677f5cfa4f6
Katya-Kuzenko <keyti.keuz@gmail.com>
2023-12-10 20:46:12 +0100

    add task 3

--- main.hs
+++ main.hs
@@ -1,8 +1,3 @@
-{-
-1. Напишіть функцію bin2int перетворення бінарного числа заданого рядком у число. Наприклад:
-bin2int “000110” => 6 
--}
-
 -- 1
 bin2int :: String -> Int
 bin2int = foldl (\acc x -> acc * 2 + read [x]) 0
@@ -10,3 +5,23 @@ bin2int = foldl (\acc x -> acc * 2 + read [x]) 0
 task1 = do
   print "Task 1"
   print $ bin2int "000110"  -- 6
+
+{-
+3. Напишіть функцію splitBy :: String -> String -> [String] 
+розбиття заданого рядка на підрядки заданою множиною символів:
+splitBy “,” “Hello, World!” => [“Hello”, “ World!”]
+-}
+
+-- 3
+
+splitBy :: String -> String -> [String]
+splitBy delimiter [] = []
+splitBy delimiter str =
+  let (start, rest) = span (`elem` delimiter) str
+  in case rest of
+    [] -> [start]
+    _ -> start : splitBy delimiter (tail rest)
+
+task2 = do -- воно виводить якусь фігню:) 
+  print "Task 2"
+  print $ splitBy "," "Hello, World!" -- [“Hello”, “ World!”]
