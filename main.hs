c47064bf75ffecc433764f3517298478aa493f44
Katya-Kuzenko <keyti.keuz@gmail.com>
2023-12-10 21:55:15 +0100

    add task 6

--- main.hs
+++ main.hs
@@ -32,5 +32,23 @@ search tree = search' [tree]
     search' (Node v l r : rest) = v : search' (rest ++ [l, r])
 
 task5 = do
+  print "Task 5"
   let tree = Node 1 (Node 2 (Node 4 Empty Empty) (Node 5 Empty Empty)) (Node 3 (Node 6 Empty Empty) Empty)
   print $ search tree
+
+{-
+6. Напишіть функцію, що для заданого списку пар чисел, 
+що представляють точки площини, знаходить максимальну відстань між точками.
+-}
+
+-- відстань = корінь ( (x2 - x1)^2 + (y2 - y1)^2 )
+-- 6 
+
+dist :: (Double, Double) -> (Double, Double) -> Double
+dist (x1, y1) (x2, y2) = sqrt ((x2 - x1)^2 + (y2 - y1)^2)
+maxDist :: [(Double, Double)] -> Double
+maxDist a = maximum [dist a1 a2 | a1 <- a, a2 <- a]
+
+task6 = do
+  print "Task 6"
+  print $ maxDist [(1, 2), (3, 4), (5, 6), (7, 8)]
