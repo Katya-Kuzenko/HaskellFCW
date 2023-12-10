5d0dd615f1309bd44640ffc6339ac1c91ec924e2
Katya-Kuzenko <keyti.keuz@gmail.com>
2023-12-10 21:26:55 +0100

    add task 5; add correct task 3

--- main.hs
+++ main.hs
@@ -6,22 +6,31 @@ task1 = do
   print "Task 1"
   print $ bin2int "000110"  -- 6
 
-{-
-3. Напишіть функцію splitBy :: String -> String -> [String] 
-розбиття заданого рядка на підрядки заданою множиною символів:
-splitBy “,” “Hello, World!” => [“Hello”, “ World!”]
--}
-
 -- 3
-
 splitBy :: String -> String -> [String]
 splitBy delimiter [] = []
 splitBy delimiter str =
-  let (start, rest) = span (`elem` delimiter) str
-  in case rest of
-    [] -> [start]
-    _ -> start : splitBy delimiter (tail rest)
+  case dropWhile (`elem` delimiter) str of
+    "" -> []
+    rest -> start : splitBy delimiter (drop (length start + length delimiter) str)
+      where (start, _) = break (`elem` delimiter) str
 
-task2 = do -- воно виводить якусь фігню:) 
+task3 = do 
   print "Task 2"
   print $ splitBy "," "Hello, World!" -- [“Hello”, “ World!”]
+
+
+-- 5
+data Tree a = Node a (Tree a) (Tree a) | Empty
+
+search :: Tree a -> [a]
+search tree = search' [tree]
+  where 
+    search' :: [Tree a] -> [a]
+    search' [] = []
+    search' (Empty : rest) = search' rest
+    search' (Node v l r : rest) = v : search' (rest ++ [l, r])
+
+task5 = do
+  let tree = Node 1 (Node 2 (Node 4 Empty Empty) (Node 5 Empty Empty)) (Node 3 (Node 6 Empty Empty) Empty)
+  print $ search tree
